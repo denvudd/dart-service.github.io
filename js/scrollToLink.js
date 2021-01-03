fb63e9c6fbfdb828c533b04a2fe388574e289a67
denvudd <68691654+denvudd@users.noreply.github.com>
2021-01-03 14:40:26 +0200

    Add files via upload

---
+++ js/scrollToLink.js
@@ -0,0 +1,19 @@
+$(function() {
+  $("[data-scroll]").on("click", function(event) {
+      event.preventDefault();
+      
+      let elementId = $(this).data('scroll');
+      let elementOffset = $(elementId).offset().top;
+      
+      var $this = $(this),
+          blockId = $this.data('scroll'),
+          blockOffset = $(blockId).offset().top;
+      
+      $('#nav a').removeClass("active");
+      $this.addClass("active")
+      
+      $("html, body").animate({
+          scrollTop: elementOffset -70
+      }, 1000);
+  });
+});

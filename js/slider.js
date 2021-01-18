232c9dee74aa215597e76001b74cabc295511694
denvudd <68691654+denvudd@users.noreply.github.com>
2021-01-18 18:00:15 +0200

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,14 @@
 $(document).ready(function (){
   $('.slider').slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    verticalDraggable: true,
-    verticalSwiping: true,
-    waitForAnimate: true,
-    variableWidth: 760,
-    vertical: true,
+    slidesToShow:         2,
+    slidesToScroll:       2,
+    autoplay:             true,
+    autoplaySpeed:        5000,
+    verticalDraggable:    true,
+    verticalSwiping:      true,
+    waitForAnimate:       true,
+    variableWidth:        760,
+    vertical:             true,
     responsive: [
       {
         breakpoint: 1024,
@@ -25,29 +25,29 @@ $(document).ready(function (){
     ]
   });
   $('.team__slider').slick({
-   slidesToShow: 3,
-   slidesToScroll: 1,
-   centerMode: true,
-   centerPadding: '10px',
-   draggable: false,
-   swiping: false,
-   autoplay: true,
-   autoplaySpeed: 5000,
+   slidesToShow:          3,
+   slidesToScroll:        1,
+   centerMode:            true,
+   centerPadding:         '10px',
+   draggable:             false,
+   swiping:               false,
+   autoplay:              true,
+   autoplaySpeed:         5000,
    responsive: [
     {
       breakpoint: 900,
       settings: {
-        veriableWidth: 560,
-        arrows: true,
-        swaping: true,
+        veriableWidth:    560,
+        arrows:           true,
+        swaping:          true,
       }
     },
     {
       breakpoint: 450,
       settings: {
-        slidesToShow: 1,
-        touchThreshold: 10,
-        swipeToSlide: true,
+        slidesToShow:     1,
+        touchThreshold:   10,
+        swipeToSlide:     true,
       }
     }
   ]
